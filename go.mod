37ca6eefe6cac927d4eea072c8a005171ec93002
Ryan M. Howell <rmhowell@protonmail.com>
2023-09-02 00:55:16 -0700

    WIP

--- go.mod
+++ go.mod
@@ -2,4 +2,17 @@ module gin
 
 go 1.14
 
-require github.com/gin-gonic/gin v1.7.1 // indirect
+require (
+	firebase.google.com/go/v4 v4.12.0 // indirect
+	github.com/bytedance/sonic v1.10.0 // indirect
+	github.com/gin-contrib/cors v1.4.0 // indirect
+	github.com/gin-gonic/gin v1.9.1 // indirect
+	github.com/go-playground/validator/v10 v10.15.3 // indirect
+	github.com/klauspost/cpuid/v2 v2.2.5 // indirect
+	github.com/pelletier/go-toml/v2 v2.1.0 // indirect
+	go.mongodb.org/mongo-driver v1.12.1 // indirect
+	golang.org/x/arch v0.4.0 // indirect
+	golang.org/x/net v0.14.0 // indirect
+	google.golang.org/api v0.138.0 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,22 +1,65 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"math/rand"
+	"os"
 	"time"
 )
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
-
 	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
-		phrases := []string{"Hello world!", "Hi there!", "Greetings!", "Salutations!", "Hey!"}
-		randomIndex := rand.Intn(len(phrases))
 
-		c.JSON(200, gin.H{
-			"message": phrases[randomIndex],
-		})
+	client, err := mongo.NewClient(options.Client().ApplyURI(os.Getenv("MONGO_URI")))	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer client.Disconnect(ctx)
+
+	collection := client.Database("test").Collection("phrases")
+
+	r.GET("/", func(c *gin.Context) {
+		reqCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		cursor, err := collection.Find(reqCtx, bson.M{})
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		var phrases []bson.M
+		err = cursor.All(reqCtx, &phrases)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		if len(phrases) > 0 {
+			randomIndex := rand.Intn(len(phrases))
+			randomPhrase := phrases[randomIndex]
+
+			c.JSON(200, gin.H{
+				"message": randomPhrase["phrase"],
+			})
+		} else {
+			c.JSON(200, gin.H{
+				"message": "No phrases in the database",
+			})
+		}
 	})
+
 	r.Run()
 }
